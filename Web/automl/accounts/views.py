eb21cfcc2676bec9f7971fd5f212b78868182bec
nguyenlz <146945072+nguyenlz@users.noreply.github.com>
2024-05-11 13:19:11 +0700

    signin-up  ui

--- Web/automl/accounts/views.py
+++ Web/automl/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'accounts/homepage.html')
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+def signin(request):
+    return render(request, 'accounts/signin.html')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
--- Web/automl/automl/urls.py
+++ Web/automl/automl/urls.py
@@ -15,18 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home Page')
-
-def contact(request):
-    return HttpResponse('Contact Page')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', contact),
+    path('', include('accounts.urls')),
 ]
