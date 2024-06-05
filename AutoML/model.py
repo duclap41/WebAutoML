a7f0b5a7438216ed636a311836049a655e2ea2b3
duc-lap <rumbonaruson@gmail.com>
2024-06-05 16:43:37 +0700

    Add regression models

--- AutoML/model.py
+++ AutoML/model.py
@@ -1,3 +1,5 @@
+import base64
+import io
 import os
 import pandas as pd
 from pandas.core.frame import DataFrame
@@ -21,12 +23,25 @@ class Model:
         df_preprocess_info = classification.pull()
         best_model = classification.compare_models()
         df_compare = classification.pull()
+
         if save_model:
             classification.save_model(best_model, os.path.join(save_path, 'best_model'))
 
         return df_preprocess_info, df_compare
 
-if __name__ == '__main__':
-    m = Model(pd.read_csv('../Data/vehicle.csv'))
-    pre, df = m.classify_models(target_col="Vehicle_Mass")
-    print(pre)
+    def regressor_models(self, target_col, save_model=False, save_path=any):
+        """Compare all classifier models in pycaret, take model give best result"""
+        regression.setup(data=self._df, target=target_col,
+                             remove_outliers=True,
+                            imputation_type='simple',
+                            numeric_imputation='mean',
+                            categorical_imputation='mode',
+                            use_gpu=False)
+        df_preprocess_info = regression.pull()
+        best_model = regression.compare_models()
+        df_compare = regression.pull()
+
+        if save_model:
+            regression.save_model(best_model, os.path.join(save_path, 'best_model'))
+
+        return df_preprocess_info, df_compare
