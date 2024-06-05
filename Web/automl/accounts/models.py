70775b7a7ede22a47fccdbcccc70694c7a5e0460
duc-lap <rumbonaruson@gmail.com>
2024-06-05 16:44:02 +0700

    Change Data Field

--- Web/automl/accounts/models.py
+++ Web/automl/accounts/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class UserFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='files/')
-    photo = models.BinaryField()
-    conclusion = models.TextField()
-    
+    data_file = models.FileField(upload_to='save_files/')
+    best_model = models.TextField()
+    best_result = models.FloatField(default=0.0)
+
     def __str__(self):
-        return f"{self.user.username}'s file"
+        return f"{self.user}'s file"
